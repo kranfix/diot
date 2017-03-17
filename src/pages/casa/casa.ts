f014c71bc7bf34abbce23f573128e80a73090424
Frank Moreno <frankmoreno1993@gmail.com>
2017-03-17 18:22:21 -0500

    Casa ya apunta a Sala y Dormitorio

--- src/pages/casa/casa.ts
+++ src/pages/casa/casa.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { SalaPage } from '../sala/sala';
+import { DormiPage } from '../dormi/dormi';
 
 @Component({
   selector: 'page-casa',
   templateUrl: 'casa.html'
 })
 export class CasaPage {
+  RootSala: any = SalaPage;
+  RootDormi: any = DormiPage;
+
+  places = [
+    {
+      "room":'Sala',
+      "page":SalaPage
+    },{
+      "room":'Dormitorio',
+      "page":DormiPage
+    },
+  ];
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goToPage(OtherPage) {
+    this.navCtrl.push(OtherPage);
   }
 
 }
